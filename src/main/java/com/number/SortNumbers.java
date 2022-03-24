0b624bfb8e1e2e34c69f4a40b8112c405b823721
Grisha <grisha2@bk.ru>
2022-03-24 21:40:22 +0300

    commit

--- src/main/java/com/number/SortNumbers.java
+++ src/main/java/com/number/SortNumbers.java
@@ -1,20 +1,23 @@
 package com.number;
 
 
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 import java.io.*;
 import java.util.Comparator;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
  * @author Grigory Yakovlev
  */
 public class SortNumbers {
+    static Integer oldValue = 0;
     public static void main(String[] args) {
-        String fileName = "/mnt/3696B2E796B2A731/job/efin/efin/configurations.properties";
-        int oldNumber;
+        String fileName = "/home/grisha/Загрузки/configurations.properties";
 
         try {
             File file = new File(fileName);
@@ -30,8 +33,16 @@ public class SortNumbers {
             String line = reader.readLine();
             while (line != null) {
                 int pointIndex = line.indexOf(".") + 1;
-                String num = line.substring(pointIndex, pointIndex + 3);
-                oldNumber = Integer.parseInt(num);
+                String num = getNumber(line);
+                if(!num.equals("")) {
+                    int numValue = Integer.parseInt(num);
+                    if (oldValue == 0) {
+                        oldValue = numValue;
+                    } else {
+                        line = calculateStr(numValue, oldValue, line);
+                    }
+                }
+
                 buffer.append(line);
                 buffer.append(System.lineSeparator());
                 System.out.println(line);
@@ -48,9 +59,47 @@ public class SortNumbers {
         }
     }
 
-    public String getNumber(String str){
-        String regex = "^efin.";
+    public static String getNumber(String str) {
+        String regex = "\\d{3}";
         Pattern pattern = Pattern.compile(regex);
-        return str;
+        Matcher match = pattern.matcher(str);
+        String number = "";
+        while (match.find()) {
+            int st = match.start();
+            int en = match.end();
+            number = str.substring(st, en);
+        }
+        return number;
+    }
+
+    public static String calculateStr(int newNum, int oldNum, String line){
+        String pars;
+        boolean isGood = oldNum + 1 == newNum;
+        if(isGood){
+            pars = transformInStringNumber(newNum);
+            oldValue = newNum;
+        }else{
+            pars = transformInStringNumber(oldNum + 1);
+            oldValue = oldNum + 1;
+        }
+
+
+
+        String regex = "\\d{3}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher match = pattern.matcher(line);
+        match.find();
+        String newLine = match.replaceFirst(pars);
+        return newLine;
+    }
+
+    public static String transformInStringNumber(int value){
+        String pars = String.valueOf(value);
+        if(pars.length() < 2){
+            pars = "00" + pars;
+        }else if(pars.length() < 3){
+            pars = "0" + pars;
+        }
+        return pars;
     }
 }
